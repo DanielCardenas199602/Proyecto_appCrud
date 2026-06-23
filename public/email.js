56f2c6de4f45243625750bf159a0d6190e72c454
Daniel Cardenas <gizmomaniac131@gmail.com>
2026-06-22 19:05:04 -0600

    smtp puerto 465 ssl

--- public/email.js
+++ public/email.js
@@ -1,14 +1,14 @@
 const envioBTN = document.getElementById('envio');
 const form = document.getElementById('formCorreo');
 
- const BASE_URL = window.API_URL;
- 
+const BASE_URL = window.API_URL;
+
 envioBTN.addEventListener('click', async (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
 
-   // 🔒 Desactivar el botón y mostrar "Enviando..."
+  // 🔒 Desactivar el botón y mostrar "Enviando..."
   envioBTN.disabled = true;
   envioBTN.textContent = "Enviando...";
 
@@ -20,14 +20,15 @@ envioBTN.addEventListener('click', async (event) => {
     });
 
     const data = await response.json();
+    console.log("Respuesta completa:", JSON.stringify(data));
     alert(data.mensaje);
 
     if (data.vista) {
       const link = document.createElement("a");
       link.href = data.vista;
       link.textContent = "Ver correo en Ethereal";
-     window.open(data.vista, "_blank");
-        document.body.appendChild(link);
+      window.open(data.vista, "_blank");
+      document.body.appendChild(link);
     }
 
     form.reset();
