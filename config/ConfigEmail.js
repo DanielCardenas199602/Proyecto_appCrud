ea2949beda9192bd0fb3d97d0234c3b085b1a037
Daniel Cardenas <gizmomaniac131@gmail.com>
2026-06-26 17:09:16 -0600

    revertir ConfigEmail original

--- config/ConfigEmail.js
+++ config/ConfigEmail.js
@@ -23,7 +23,7 @@ async function CrateTranporte() {
         return nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
