31396e5db84b34b29e848f43f48f626a326c6d6a
Daniel Cardenas <gizmomaniac131@gmail.com>
2026-07-16 15:37:00 -0600

    fix import ControladorRegistro en rutas

--- routes/rutas.js
+++ routes/rutas.js
@@ -7,7 +7,7 @@ const path = require("path");//importamos path para manejar rutas de archivos
 const ProyectController = require("../controllers/ControladorCrud");//importamos el controlador para manejar la lógica de las rutas
 const upload = require("../middlewares/uploads");
 const { Enviarcorreo } = require("../controllers/ControladorEmail");//importamos el controlador para manejar la lógica de las rutas de correo
-const { obtenerRegistros, eliminarRegistro, eliminarTodosRegistros } = require("../controllers/ContrladorRegistro");
+const { obtenerRegistro, eliminarRegistro, eliminarTodosRegistros } = require("../controllers/ContrladorRegistro");
 
 
 // Ruta GET de prueba
